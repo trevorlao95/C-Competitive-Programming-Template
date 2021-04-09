dd6041b8562832e7be10b0a84d7d46f9165685ae
Trevor Lao <trevor@karbonhq.com>
2021-04-10 00:24:09 +1000

    feat(solutions): added solutions

---
+++ CSharpCompProgrammingTemplate.Solutions/Leetcode/62. Unique Paths.cs
@@ -0,0 +1,48 @@
+﻿using CSharpCompProgrammingTemplate.Helpers;
+using System.Collections.Generic;
+
+namespace CSharpCompProgrammingTemplate
+{
+    public class Program
+    {
+        #region Main
+
+        private static void Main(string[] args)
+        {
+            QuestionHelpers.Time(() => new Solution().UniquePaths(LeetCode.Int(), LeetCode.Int(1)));
+        }
+
+        #endregion Main
+
+        #region Solution
+
+        public class Solution
+        {
+            public int UniquePaths(int m, int n)
+            {
+                var uniqueWays = new List<List<int>>();
+
+                for (int i = 0; i < m; i++)
+                {
+                    var uniqueWaysRow = new List<int>();
+
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (i == 0)
+                            uniqueWaysRow.Add(1);
+                        else if (j == 0)
+                            uniqueWaysRow.Add(1);
+                        else
+                            uniqueWaysRow.Add(uniqueWaysRow[j - 1] + uniqueWays[i - 1][j]);
+                    }
+
+                    uniqueWays.Add(uniqueWaysRow);
+                }
+
+                return uniqueWays[m - 1][n - 1];
+            }
+        }
+
+        #endregion Solution
+    }
+}
---
+++ CSharpCompProgrammingTemplate.Solutions/Leetcode/64. Minimum Path Sum.cs
@@ -0,0 +1,51 @@
+﻿using CSharpCompProgrammingTemplate.Helpers;
+using System;
+using System.Collections.Generic;
+
+namespace CSharpCompProgrammingTemplate
+{
+    public class Program
+    {
+        #region Main
+
+        private static void Main(string[] args)
+        {
+            QuestionHelpers.Time(() => new Solution().MinPathSum(LeetCode.Grid()));
+        }
+
+        #endregion Main
+
+        #region Solution
+
+        public class Solution
+        {
+            public int MinPathSum(int[][] grid)
+            {
+                List<List<int>> shortestPathGrid = new List<List<int>>();
+
+                for (int i = 0; i < grid.Length; i++)
+                {
+                    var row = new List<int>();
+
+                    for (int j = 0; j < grid[i].Length; j++)
+                    {
+                        if (i == 0 && j == 0)
+                            row.Add(grid[i][j]);
+                        else if (i == 0)
+                            row.Add(grid[i][j] + row[j - 1]);
+                        else if (j == 0)
+                            row.Add(grid[i][j] + shortestPathGrid[i - 1][j]);
+                        else
+                            row.Add(Math.Min(grid[i][j] + shortestPathGrid[i - 1][j], grid[i][j] + row[j - 1]));
+                    }
+
+                    shortestPathGrid.Add(row);
+                }
+
+                return shortestPathGrid[grid.Length - 1][grid[0].Length - 1];
+            }
+        }
+
+        #endregion Solution
+    }
+}
