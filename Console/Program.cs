50a976449d3676a1bb7e07c8300967ddd08e8599
Trevor Lao <trevor@karbonhq.com>
2021-04-10 00:25:21 +1000

    feat(console): overall file cleanup

---
+++ Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using CSharpCompProgrammingTemplate.Helpers;
+using System.Collections.Generic;
+
+namespace CSharpCompProgrammingTemplate
+{
+    public class Program
+    {
+        #region Main
+
+        private static void Main(string[] args)
+        {
+            QuestionHelpers.Time(() => new Solution().NumDecodings(LeetCode.String()));
+        }
+
+        #endregion Main
+
+        #region Solution
+
+        public class Solution
+        {
+            public int NumDecodings(string s)
+            {
+            }
+        }
+
+        #endregion Solution
+    }
+}
